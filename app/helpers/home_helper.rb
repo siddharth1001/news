4c5da5659d693fcf875ca1e5d432fc8a4f443c24
siddharthr_qubole <siddharthr@qubole.com>
2020-10-15 00:00:50 +0530

    rss

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,16 @@
 module HomeHelper
+
+  def articles(query="India")
+    require 'rss'
+    require 'open-uri'
+    url = "https://news.google.com/rss/search?q=#{query}"
+    open(url) do |rss|
+      RSS::Parser.parse(rss)
+      # feed = RSS::Parser.parse(rss)
+      # puts "Title: #{feed.channel.title}"
+      # feed.items.each do |item|
+      #   puts "Item: #{item.title}"
+      # end
+    end
+  end
 end
