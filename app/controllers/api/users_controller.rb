3d290b8bbc3f0f76071c39eccfb1ee9644efc0cb
siddharth1001 <siddharth.rawat1001@gmail.com>
2020-10-19 16:05:30 +0530

    protect_from_forgery to turn off csrf token verfy

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
-    def index
-        response_data = {controller: "User123"}
-        render json: response_data
-    end
+  def index
+    response_data = {controller: "User123"}
+    render json: response_data
+  end
 end
