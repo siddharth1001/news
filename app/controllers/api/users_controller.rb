ec3f73cdc1a2874d42e1f5fe65969dc5a9b87538
siddharth1001 <siddharth.rawat1001@gmail.com>
2020-10-19 15:49:40 +0530

    add users controller with versioning

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,6 @@
+class Api::UsersController < ApplicationController
+    def index
+        response_data = {controller: "User123"}
+        render json: response_data
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
   get 'home/index'
+
+  namespace :api do
+
+    ["v1"].each do |version|
+      namespace version do
+        resources :users
+        get 'test/index'
+      end
+    end
+
+    ["v2"].each do |version|
+      namespace version do
+        get 'test/index'
+      end
+    end
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
